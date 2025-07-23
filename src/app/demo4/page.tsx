d5c982993f5c5a001d28b85b4686011acdd54e58
naoya5 <naoya2115@outlook.jp>
2025-07-23 12:09:27 +0900

    Cache の問題を解消

--- src/app/demo4/page.tsx
+++ src/app/demo4/page.tsx
@@ -1,6 +1,8 @@
 import { getUsers } from "@/server/dal/user";
 import UserSection from "./_components/user-section";
 
+export const dynamic = "force-dynamic";
+
 async function Page() {
   const users = await getUsers({ id: "asc" });
 
